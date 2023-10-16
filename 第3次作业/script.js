f5fe6b848748d7f439ea1f3efa913bac71531ae8
MissXia07 <2143461541@qq.com>
2023-10-16 15:24:18 +0800

    Update

---
+++ 第3次作业/script.js
@@ -0,0 +1,8 @@
+// 使用getElementById获取元素  
+var element = document.getElementById("test");  
+  
+// 输出元素的HTML内容  
+console.log(element.innerHTML);   
+  
+// 修改元素的HTML内容  
+element.innerHTML = "I HAVE CHANGED";
